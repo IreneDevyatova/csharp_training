2dc297edf3f7e5fa6768d7e746f64f8832f2ba28
Irene <irene.devyatova@gmail.com>
2017-04-09 10:42:43 +0300

    New entry creation test

--- addressbook-web-tests/addressbook-web-tests/EntryCreationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/EntryCreationTests.cs
@@ -10,7 +10,7 @@ using OpenQA.Selenium.Support.UI;
 namespace WebAddressbookTests
 {
     [TestFixture]
-    public class GroupCreationTests
+    public class EntryCreationTests
     {
         private IWebDriver driver;
         private StringBuilder verificationErrors;
@@ -24,7 +24,7 @@ namespace WebAddressbookTests
             baseURL = "http://localhost";
             verificationErrors = new StringBuilder();
         }
-        
+
         [TearDown]
         public void TeardownTest()
         {
@@ -40,48 +40,40 @@ namespace WebAddressbookTests
         }
 
         [Test]
-        public void GroupCreationTest()
+        public void EntryCreationTest()
         {
             OpenHomePage();
             Login(new AccountData("admin", "secret"));
-            GoToGroupsPage();
-            InitGroupCreation();
-            GroupData group = new GroupData("aaa");
-            group.Header = "ddd";
-            group.Footer = "sss";
-            FillInGroupForm(group);
-            SubmitGroupCreation();
-            ReturnToGroupsPage();
+            GoToCreateEntryPage();
+            EntryData entry = new EntryData("irene", "devyatova");
+            FillInEntryForm(entry);
+            SubmitEntryCreation();
+            // ERROR: Caught exception [Error: Dom locators are not implemented yet!]
+            ReturnToHomePage();
         }
 
-        private void ReturnToGroupsPage()
+        private void ReturnToHomePage()
         {
-            driver.FindElement(By.LinkText("group page")).Click();
+            driver.FindElement(By.LinkText("home page")).Click();
         }
 
-        private void SubmitGroupCreation()
+        private void FillInEntryForm(EntryData entry)
         {
-            driver.FindElement(By.Name("submit")).Click();
+            driver.FindElement(By.Name("firstname")).Clear();
+            driver.FindElement(By.Name("firstname")).SendKeys(entry.Firstname);
+            driver.FindElement(By.Name("lastname")).Clear();
+            driver.FindElement(By.Name("lastname")).SendKeys(entry.Lastname);
+           
         }
 
-        private void FillInGroupForm(GroupData group)
+        private void SubmitEntryCreation()
         {
-            driver.FindElement(By.Name("group_name")).Clear();
-            driver.FindElement(By.Name("group_name")).SendKeys(group.Name);
-            driver.FindElement(By.Name("group_header")).Clear();
-            driver.FindElement(By.Name("group_header")).SendKeys(group.Header);
-            driver.FindElement(By.Name("group_footer")).Clear();
-            driver.FindElement(By.Name("group_footer")).SendKeys(group.Footer);
+            driver.FindElement(By.CssSelector("input[type=\"submit\"]")).Click();
         }
 
-        private void InitGroupCreation()
+        private void GoToCreateEntryPage()
         {
-            driver.FindElement(By.Name("new")).Click();
-        }
-
-        private void GoToGroupsPage()
-        {
-            driver.FindElement(By.LinkText("groups")).Click();
+            driver.FindElement(By.LinkText("add new")).Click();
         }
 
         private void Login(AccountData account)
@@ -93,7 +85,6 @@ namespace WebAddressbookTests
             driver.FindElement(By.CssSelector("input[type=\"submit\"]")).Click();
         }
 
-        
         private void OpenHomePage()
         {
             driver.Navigate().GoToUrl(baseURL + "/addressbook/");
@@ -148,3 +139,4 @@ namespace WebAddressbookTests
         }
     }
 }
+
